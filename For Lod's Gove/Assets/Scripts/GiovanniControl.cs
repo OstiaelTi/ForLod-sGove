04f24d0c71b4b9d112cfb250e1cadbabc51c292f
SrImportante <bernatpont98@gmail.com>
2017-04-22 18:01:58 +0200

    Giovanni cubo y Diablillo triangulo
    Conrol del jugador i equació d'interpolació lineal per seguir-lo.

---
+++ For Lod's Gove/Assets/Scripts/GiovanniControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GiovanniControl : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+
+//Booleans per controlar el moviment
+    private bool moveUp;
+    private bool moveDown;
+    private bool moveRight;
+    private bool moveLeft;
+
+    private Rigidbody2D rb2d;
+    private Vector2 movement;
+
+  
+
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+ 
+
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(
+            moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime,
+            moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 
+            0f);
+        
+        
+        /*moveUp = Input.GetKey("up");
+        moveDown = Input.GetKey("down");
+        moveRight = Input.GetKey("right");
+        moveLeft = Input.GetKey("left");
+
+        if (moveUp)
+        {movement = new Vector2(0, moveSpeed);}
+        if (moveDown)
+        { movement = new Vector2(0, -moveSpeed); }
+        if (moveRight)
+        { movement = new Vector2(moveSpeed, 0); }
+        if (moveLeft)
+        { movement = new Vector2(-moveSpeed, 0); }
+        
+
+        
+        rb2d.AddForce(movement);*/
+        
+    }
+
+}
---
+++ For Lod's Gove/Assets/Scripts/IA_Diablillo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class IA_Diablillo : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public int life;
+
+    public Transform target;
+
+    //posicio inicial
+    public int x, y;
+
+
+    private Rigidbody2D rb2d;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.position = new Vector2(x, y);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(
+            target.position.x * moveSpeed + (1 - moveSpeed) * transform.position.x, 
+            target.position.y * moveSpeed + (1 - moveSpeed) * transform.position.y
+            );
+    }
+
+}
