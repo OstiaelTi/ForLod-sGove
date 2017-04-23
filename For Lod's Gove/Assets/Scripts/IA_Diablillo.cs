de53518fe6a4d4cdc62472a18cb408f7f2201fd1
SrImportante <bernatpont98@gmail.com>
2017-04-23 18:46:49 +0200

    Rocas
    objectes obstacle amb el seu tag "obstacle" i intenta de esquivar
    "inteligentment", segueixo explorant

--- For Lod's Gove/Assets/Scripts/IA_Diablillo.cs
+++ For Lod's Gove/Assets/Scripts/IA_Diablillo.cs
@@ -10,6 +10,10 @@ public class IA_Diablillo : MonoBehaviour
     public int life;
 
     public Transform target;
+    public Transform obstacle;
+
+    public float disToChangeDirecction;
+
 
     //posicio inicial
     public int x, y;
@@ -22,7 +26,10 @@ public class IA_Diablillo : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        target = GameObject.FindGameObjectWithTag("Obsacle").transform;
+
         transform.position = new Vector2(x, y);
     }
 
@@ -34,6 +41,20 @@ public class IA_Diablillo : MonoBehaviour
             target.position.x * moveSpeed + (1 - moveSpeed) * transform.position.x, 
             target.position.y * moveSpeed + (1 - moveSpeed) * transform.position.y
             );
+        if ((-transform.position.x + obstacle.position.x < disToChangeDirecction) || (-transform.position.y + obstacle.position.y < disToChangeDirecction))
+        {
+            transform.position = new Vector2(
+            -(target.position.x * moveSpeed + (1 - moveSpeed) * transform.position.x),
+            -(target.position.y * moveSpeed + (1 - moveSpeed) * transform.position.y)
+            );
+        }
     }
 
 }
+
+/*
+ transform.position = new Vector2(
+    ((target.position.x / Mathf.Sqrt(Mathf.Pow(target.position.x, 2) + Mathf.Pow(target.position.y, 2))) * moveSpeed) + ((1 - moveSpeed) * (transform.position.x / Mathf.Sqrt(Mathf.Pow(transform.position.x, 2) + Mathf.Pow(transform.position.y, 2)))),
+    ((target.position.y / Mathf.Sqrt(Mathf.Pow(target.position.x, 2) + Mathf.Pow(target.position.y, 2))) * moveSpeed) + ((1 - moveSpeed) * (transform.position.y / Mathf.Sqrt(Mathf.Pow(transform.position.x, 2) + Mathf.Pow(transform.position.y, 2))))
+    );
+*/
