a5ba2de08b6ecc5d483cefe28e75b02aabae5c22
OstiaelTi <melindrillo@hotmail.com>
2017-06-20 21:41:19 +0200

    spawn

--- For Lod's Gove/Assets/Scripts/Room_1.cs
+++ For Lod's Gove/Assets/Scripts/Room_1.cs
@@ -6,20 +6,71 @@ public class Room_1 : MonoBehaviour {
 
     public GameObject Diablillo;
     public GameObject Rock;
-
+	public GameObject Fire;
+	public int roomTypes;
 
     // Use this for initialization
-    void Awake () {
+    void Awake () 
+		{
+		float s = Random.Range (0, (roomTypes+1));
+		int sw = (int)s;
+			switch (sw) {
+		case 0:
+			Instantiate (Rock, new Vector2 (transform.position.x + 10, transform.position.y), Quaternion.identity);
+			Instantiate (Rock, new Vector2 (transform.position.x - 10, transform.position.y), Quaternion.identity);
+			Instantiate (Rock, new Vector2 (transform.position.x, transform.position.y + 10), Quaternion.identity);
+			Instantiate (Rock, new Vector2 (transform.position.x, transform.position.y - 10), Quaternion.identity);
+
+			Instantiate (Diablillo, new Vector2 (transform.position.x - 10, transform.position.y - 10), Quaternion.identity);
+			Instantiate (Diablillo, new Vector2 (transform.position.x + 10, transform.position.y + 10), Quaternion.identity);
+			break;
+
+		case 2:
+
+			Instantiate (Fire, new Vector2 (transform.position.x + 10, transform.position.y), Quaternion.identity);
+			Instantiate (Fire, new Vector2 (transform.position.x - 10, transform.position.y), Quaternion.identity);
+			Instantiate (Fire, new Vector2 (transform.position.x, transform.position.y + 10), Quaternion.identity);
+			Instantiate (Fire, new Vector2 (transform.position.x, transform.position.y - 10), Quaternion.identity);
+			Instantiate (Diablillo, new Vector2 (transform.position.x , transform.position.y), Quaternion.identity);
+			break;
+
+		case 3:
+			
+			Instantiate (Rock, new Vector2 (transform.position.x + 10, transform.position.y+ 10), Quaternion.identity);
+			Instantiate (Rock, new Vector2 (transform.position.x - 10, transform.position.y- 10), Quaternion.identity);
+			Instantiate (Fire, new Vector2 (transform.position.x- 10, transform.position.y + 10), Quaternion.identity);
+			Instantiate (Fire, new Vector2 (transform.position.x+ 10, transform.position.y - 10), Quaternion.identity);
+			Instantiate (Diablillo, new Vector2 (transform.position.x , transform.position.y), Quaternion.identity);
+
+			break;
 
 
-        Instantiate(Rock, new Vector2(transform.position.x + 10, transform.position.y), Quaternion.identity);
-        Instantiate(Rock, new Vector2(transform.position.x - 10, transform.position.y), Quaternion.identity);
-        Instantiate(Rock, new Vector2(transform.position.x, transform.position.y + 10), Quaternion.identity);
-        Instantiate(Rock, new Vector2(transform.position.x, transform.position.y - 10), Quaternion.identity);
+		case 4:
+			Instantiate (Diablillo, new Vector2 (transform.position.x + 10, transform.position.y+ 10), Quaternion.identity);
+			Instantiate (Diablillo, new Vector2 (transform.position.x - 10, transform.position.y- 10), Quaternion.identity);
+			Instantiate (Diablillo, new Vector2 (transform.position.x- 10, transform.position.y + 10), Quaternion.identity);
+			Instantiate (Diablillo, new Vector2 (transform.position.x+ 10, transform.position.y - 10), Quaternion.identity);
+			break;
 
-        Instantiate(Diablillo, new Vector2(transform.position.x - 10, transform.position.y - 10), Quaternion.identity);
-        Instantiate(Diablillo, new Vector2(transform.position.x + 10, transform.position.y + 10), Quaternion.identity);
-    }
+		case 5:
+			Instantiate (Diablillo, new Vector2 (transform.position.x + 10, transform.position.y+10), Quaternion.identity);
+			Instantiate (Diablillo, new Vector2 (transform.position.x - 10, transform.position.y-10), Quaternion.identity);
+			Instantiate (Fire, new Vector2 (transform.position.x , transform.position.y), Quaternion.identity);
+
+			break;
+
+		default:
+			Instantiate (Rock, new Vector2 (transform.position.x + 10, transform.position.y), Quaternion.identity);
+			Instantiate (Rock, new Vector2 (transform.position.x - 10, transform.position.y), Quaternion.identity);
+			Instantiate (Rock, new Vector2 (transform.position.x, transform.position.y + 10), Quaternion.identity);
+			Instantiate (Rock, new Vector2 (transform.position.x, transform.position.y - 10), Quaternion.identity);
+
+			Instantiate (Diablillo, new Vector2 (transform.position.x - 10, transform.position.y - 10), Quaternion.identity);
+			Instantiate (Diablillo, new Vector2 (transform.position.x + 10, transform.position.y + 10), Quaternion.identity);
+			
+			break;
+		}
+		}
 	
 	// Update is called once per frame
 	void Update () {
---
+++ For Lod's Gove/Assets/Scripts/Spawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject Diablillo;
+	public GameObject Gordo;
+	public GameObject Fantasma;
+	public int zona;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		float s = Random.Range (0, 3);
+		int sw = (int)s;
+		switch (sw) {
+		case 0:
+			Instantiate (Diablillo, new Vector2 (transform.position.x, transform.position.y), Quaternion.identity);
+		break;
+		case 1: 
+			Instantiate (Gordo, new Vector2 (transform.position.x, transform.position.y), Quaternion.identity);
+			break;
+		case 2: 
+			Instantiate (Fantasma, new Vector2 (transform.position.x, transform.position.y), Quaternion.identity);
+			break;
+
+		default: 
+			Instantiate (Diablillo, new Vector2 (transform.position.x, transform.position.y), Quaternion.identity);
+			break;
+		
+		
+	}
+	}
+	// Update is called once per frame
+	
+}
